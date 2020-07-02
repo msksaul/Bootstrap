95c2a7a3083838c1428348b169b53bcd72668758
msksaul <64514098+msksaul@users.noreply.github.com>
2020-07-02 00:34:15 -0600

    Gulp Part 2

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,7 +3,14 @@
 let gulp = require('gulp');
 let sass = require('gulp-sass');
 let browserSync = require('browser-sync');
-
+let del = require('del');
+let imagemin = require('gulp-imagemin');
+let uglify = require('gulp-uglify');
+let usemin = require('gulp-usemin');
+let rev = require('gulp-rev');
+let cleanCss = require('gulp-flatmap');
+let htmlmin = require('gulp-htmlmin');
+let flatmap = require('gulp-flatmap')
 
 sass.compiler = require('node-sass')
 
@@ -35,3 +42,37 @@ gulp.task('default', gulp.series('browser-sync'), () => {
   gulp.parallel('sass:watch')
 });
 
+gulp.task('clean', () => {
+  return del(['dist']);
+});
+
+gulp,task('copyfonts', () => {
+  gulp.src('./node_modules/font-awesome/fonts/++/*.{ttf,woff,eof,svg}*')
+  .pipe(gulp.dest('./dist/fonts'));
+})
+
+gulp.task('imagemin', () => {
+  return gulp.src('img/*.{png,jpg,gif}')
+  .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true,}))
+  .pipe(gulp.dest('dist/img'))
+})
+
+
+gulp.task('usemin', () => {
+  return gulp.src('./*.html')
+  .pipe(flatmap(function(stream, file){
+      return stream
+        .pipe(usemin({
+            css: [ rev() ],
+            html: [ () => { return htmlmin({ collapseWhitespace: true })} ],
+            js: [ uglify(), rev() ],
+            inlinejs: [ uglify() ],
+            inlinecss: [ cleanCss(), 'concat' ]
+        }))
+    }))
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('build',gulp.series('clean'), () => {
+    gulp.parallel('copyfonts','imagemin','usemin');
+});
